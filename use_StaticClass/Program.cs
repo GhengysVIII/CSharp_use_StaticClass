c4a8f3d491e55386467595160543c61c42b0558e
GhengysVIII <34130506+GhengysVIII@users.noreply.github.com>
2020-02-20 15:20:05 +0100

    Add files via upload

---
+++ use_StaticClass/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Use_StaticClass {
+    class Program {
+        static void Main(string[] args) {
+
+            string s = "truc";
+            Console.WriteLine(s.EstPalindrome());
+
+            s = "kayak";
+            Console.WriteLine(s.EstPalindrome());
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ use_StaticClass/Tools.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Use_StaticClass {
+    public static class Tools {
+        public static bool EstPalindrome(this string str) {
+            str = str.ToLower();
+            return str == str.Reverse() ? true : false;
+        } 
+        
+        public static string Reverse(this string str) {
+            char[] reversedArray = str.ToCharArray();
+            Array.Reverse(reversedArray);
+            return new string(reversedArray);
+        }
+
+    }
+}
